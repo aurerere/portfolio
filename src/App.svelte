885e7e7a3a7a3c409672839180f14c38e29b4038
aurerere <74239193+aurerere@users.noreply.github.com>
2023-06-29 22:32:09 +0200

    ++

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,20 @@
 <script lang="ts">
-    import { Router, Route } from "svelte-routing";
-    import FormalView from "./views/FormalView.svelte";
-    import CliView from "./views/CliView.svelte";
-    import NotFoundView from "./views/NotFoundView.svelte";
+import { Router, Route } from "svelte-navigator";
+    import FormalView from "./views/FormalView/FormalView.svelte";
+    import CliView from "./views/CLIView/CliView.svelte";
+import NotFoundView from "./views/NotFoundView.svelte";
+import {onMount} from "svelte";
 
-    let url = window.location.pathname;
+let url = window.location.pathname;
+
+onMount(() => {
+    const lang = localStorage.getItem("lang");
+
+});
 </script>
 
 <Router {url}>
-    <Route path="/" component={FormalView}/>
-    <Route path="/cli" component={CliView}/>
-    <Route component={NotFoundView}/>
+    <Route path="/"><FormalView/></Route>
+    <Route path="/cli"><CliView/></Route>
+    <Route><NotFoundView/></Route>
 </Router>
--- src/views/FormalView.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-    import { Lang } from "../stores";
-
-    let lang: "fr" | "en" = "fr";
-
-    Lang.subscribe(value => lang = value);
-</script>
