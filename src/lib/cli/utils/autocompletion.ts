9018478d5d487dd85ff7cd1d2e5508207a22e0db
aurerere <74239193+aurerere@users.noreply.github.com>
2023-12-07 21:35:44 +0100

    wip autocompletion

--- src/lib/cli/utils/autocompletion.ts
+++ src/lib/cli/utils/autocompletion.ts
@@ -1,7 +1,53 @@
-export function getAutocompleteSuggestion(context: string): string[]
-{
-    let workingOn: string[] | string = context.split(/\|\||;/)
-    workingOn = workingOn[workingOn.length - 1].trimStart();
+import bin from "@cli/index";
+import {CurrentPath} from "@stores";
+import {fileTreeTraveler} from "@cli/utils/fileSystem";
 
-    return [];
+export function getAutocompleteSuggestions(input: string): string[]
+{
+    try {
+        const splitInput = input.split(/\|\||;/);
+        const context = splitInput[splitInput.length - 1].trimStart();
+
+        if (context === "") {
+            return Object.keys(bin);
+        }
+
+        let path: string[] | null = null;
+        const unsubscribe = CurrentPath.subscribe(value => path = value);
+        unsubscribe();
+
+        if (path === null)
+            return [];
+
+        if (context.endsWith(" ")) {
+            const [dest, destType] = fileTreeTraveler(path);
+
+
+        }
+
+        if (context.trim().split(" ").length > 1) {
+            /*
+            * dir => "dir/"
+            * file => "file "
+            * */
+        }
+
+        // else if (workingOn.endsWith(" ") || workingOn.split) {
+        //     let path: string[] | null = null;
+        //     const unsubscribe = CurrentPath.subscribe(value => path = value);
+        //     unsubscribe();
+        //
+        //     if (path === null)
+        //         return [];
+        //
+        //     return Object.keys()
+        // }
+
+        return [];
+    }
+    catch {
+        return [];
+    }
+
+
 }
